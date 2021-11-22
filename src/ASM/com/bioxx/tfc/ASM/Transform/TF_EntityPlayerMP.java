e7d6aebb4b3be7876fe9450790a115ecf9ca9b3b
LudwigVonChesterfield <{ID}+{username}@users.noreply.github.com>
2021-11-22 21:22:24 +0200

    Thermos requires all these values offset by 13
    https://github.com/CyberdyneCC/Thermos/issues/431

--- src/ASM/com/bioxx/tfc/ASM/Transform/TF_EntityPlayerMP.java
+++ src/ASM/com/bioxx/tfc/ASM/Transform/TF_EntityPlayerMP.java
@@ -15,16 +15,19 @@ public class TF_EntityPlayerMP extends ClassTransformer
 		mcpClassName = "net.minecraft.entity.player.EntityPlayerMP";
 		obfClassName = "mw";
 
+		// Set this to 13 for Thermos servers.
+		int offset = 0;
+
 		/**
 		 * This removes the "this.getHealth() != this.lastHealth" part of the if statement
 		 */
 		List<InstrSet> nodes = new ArrayList<InstrSet>();
-		nodes.add(new InstrSet(new LineNumberNode(0, new LabelNode()), 59, InstrOpType.Remove));
-		nodes.add(new InstrSet(new LineNumberNode(0, new LabelNode()), 60, InstrOpType.Remove));
-		nodes.add(new InstrSet(new LineNumberNode(0, new LabelNode()), 61, InstrOpType.Remove));
-		nodes.add(new InstrSet(new LineNumberNode(0, new LabelNode()), 62, InstrOpType.Remove));
-		nodes.add(new InstrSet(new LineNumberNode(0, new LabelNode()), 63, InstrOpType.Remove));
-		nodes.add(new InstrSet(new LineNumberNode(0, new LabelNode()), 64, InstrOpType.Remove));
+		nodes.add(new InstrSet(new LineNumberNode(0, new LabelNode()), 59 + offset, InstrOpType.Remove));
+		nodes.add(new InstrSet(new LineNumberNode(0, new LabelNode()), 60 + offset, InstrOpType.Remove));
+		nodes.add(new InstrSet(new LineNumberNode(0, new LabelNode()), 61 + offset, InstrOpType.Remove));
+		nodes.add(new InstrSet(new LineNumberNode(0, new LabelNode()), 62 + offset, InstrOpType.Remove));
+		nodes.add(new InstrSet(new LineNumberNode(0, new LabelNode()), 63 + offset, InstrOpType.Remove));
+		nodes.add(new InstrSet(new LineNumberNode(0, new LabelNode()), 64 + offset, InstrOpType.Remove));
 
 		mcpMethodNodes.put("onUpdateEntity | ()V", new Patch(nodes, PatchOpType.Modify));
 		obfMethodNodes.put("i | ()V", new Patch(nodes, PatchOpType.Modify));
